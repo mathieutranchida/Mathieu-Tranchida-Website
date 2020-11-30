06e7c341294fe712812865facf97784a68321941
mathieutranchida <mathieu.tranchida@gmail.com>
2020-11-29 23:50:26 -0500

    redux framework created

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,17 @@ import { Router } from "react-router";
 import history from "./history";
 
 import App from "./App";
+import { Provider } from "react-redux";
+import configureStore from "./redux/store";
+
+const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
     <Router history={history}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
---
+++ client/src/redux/reducers/defaultReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  status: "null",
+  error: null,
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+}
